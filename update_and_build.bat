3be6e7e14f5cda01bfda4b6bae35701f40f1591b
brian <brian.li@sgs.com>
2023-07-19 10:15:27 +0800

    Changes to be committed: 	new file:   update_and_build.bat 新增指令檔 update_and_build.bat - svn update 特定目錄 - MSBuild 編譯專案 - 顯示完成提示 (並直接複製到剪貼簿)

---
+++ update_and_build.bat
@@ -0,0 +1,36 @@
+@echo off
+
+:: [1] set variables
+:: local [development] path
+set DEV="C:\dev\SGS.LIMS2"
+:: local [release code] path
+set REL="D:\LIMS20\SOURCE\RELEASE_CODE"
+:: set Developer Command Prompt for VS 2022 path
+set CMD="C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat"
+:: finish message
+set MSG=本機已更新、編譯正常
+
+:: [2] SVN update
+:: SVN update all directories
+set ARR=%DEV%,%REL%
+for %%f in (%ARR%) do (
+    svn info %%f
+    svn update %%f
+echo.
+)
+
+:: [3] build solution
+:: load Developer Command Prompt for VS 2022
+call %CMD%
+:: build solution
+MSBuild %DEV%\SDO.sln /p:configuration="Release"
+
+:: [4] show message
+echo.
+echo %MSG%
+echo (訊息已複製)
+echo.
+
+:: [5] copy message
+:: use pipline send message to clipbord
+echo %MSG% | clip
