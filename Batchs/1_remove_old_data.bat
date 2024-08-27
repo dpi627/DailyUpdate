820b3790be16d64c159d8ab0986c785d395e15c3
brian_li <brian.li@sgs.com>
2024-08-27 13:48:45 +0800

    調整註解與抽換變數

--- Batchs/1_remove_old_data.bat
+++ Batchs/1_remove_old_data.bat
@@ -1,42 +1,46 @@
-@REM 可搜尋 TODO: 自行修改參數(如果有)
 @echo off
+@REM 可搜尋 TODO: 自行修改參數(如果有)
 chcp 65001 > nul
 setlocal enabledelayedexpansion
 
 @REM 參數說明
 @REM %1 {string} 專案發佈(publis)路徑 "C:\dev\_publish\RELEASE"
-@REM %2 {string} 安靜模式(執行前不再確認) y/n 預設不輸入為 n
+@REM %2 {string} 安靜模式(執行前不再確認) y/n
 
 @REM 取得外部傳遞參數
 set "PUB=%~1"
 set "SILENT=%~2"
 
-@REM 函式功能說明輸出
-echo :::: Func: Remove Last Publish Data
-echo :::: Note: 清除最後一次部署檔案，避免影響本次部署內容
+@REM 設定變數
+set "MSG_CFM=Press ENTER to use default, or input others: "
+
+@REM 功能說明
+echo :::: Remove Last Publish Data
+echo :::: 清除最後一次部署檔案，避免影響本次部署內容
 echo.
 
 @REM 設定 TODO: 專案預設發佈路徑
 if "!PUB!"=="" (
+	echo :: Set publish path
     set "PUB=C:\dev\_publish\RELEASE"
-    echo Default publis path: !PUB!
-    set /p "PUB=Press ENTER to use default, or input others: "
+    echo Default: !PUB!
+    set /p "PUB=!MSG_CFM!"
 )
+echo.
 
-@REM 如果有多個路徑可用逗點串接
+@REM 如果有多個路徑可用逗點串接，例如 "ARR=!PUB!,!XYZ!"
 set "ARR=!PUB!"
 
-@REM 確認路徑是否正確
+@REM 確認執行
 if "!SILENT!"=="" set "SILENT=n"
 if /i "!SILENT!" neq "y" (
     echo Will remove all contents in below paths...
-    echo.
     for %%f in (!ARR!) do ( echo [%%f] )
-    echo.
     choice /c yn /n /m "Press y/n: "
     if errorlevel 2 goto end
 )
 
+@REM 執行
 for %%f in (!ARR!) do (
     echo Remove all contents in [%%f]....
     if exist "%%f" (
