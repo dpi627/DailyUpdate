4a3092801fc235f9f880c79ea40198bda3694586
brian_li <brian.li@sgs.com>
2024-08-27 15:27:12 +0800

    完成編譯與發佈

--- Batchs/4_build_and_deploy.bat
+++ Batchs/4_build_and_deploy.bat
@@ -1,29 +1,53 @@
 @echo off
-:: 可搜尋 TODO，改為本機路徑，注意不要改到變數就好
-echo :::: Build and Publish RELEASE_CODE ::::
+@REM 可搜尋 TODO: 自行修改參數(如果有)
+chcp 65001 > nul
+setlocal enabledelayedexpansion
+
+@REM 參數說明
+@REM %1 {string} 專案原始碼(source code)路徑 "C:\dev\LIMS20\SOURCE\CODE"
+@REM %2 {string} 專案發佈(publis)路徑 "C:\dev\_publish\RELEASE"
+
+@REM 取得外部傳遞參數
+set "SRC=%~1"
+set "PUB=%~2"
+
+@REM 檢查傳入參數
+if "!SRC!"=="" (
+    echo Error: Source code path not specified
+    goto end
+)
+if "!PUB!"=="" (
+    echo Error: Publish path not specified
+    goto end
+)
+
+@REM 設定 TODO: 變數
+set "SLN=!SRC!\SDO.sln"
+set "SCRIPTS_DIR=!PUB!\Scripts"
+set "UPD_FILE=!SCRIPTS_DIR!\lastUpdate.js"
+
+@REM 功能說明
+echo :::: Build and Publish
+echo :::: 編譯並發佈專案，並寫入最後更新時間
 echo.
-:: Load Developer Command Prompt for VS 2022
+
+@REM Load TODO: Developer Command Prompt
 call "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat"
-:: Path to [RELEASE_CODE]
-set DIR=%1
-:: Build and Publish
-MSBuild %DIR%\SDO.sln /p:platform="any cpu" /p:configuration="release" /p:DeployOnBuild=true /p:PublishProfile="FolderProfile"
+
+@REM 編譯並發佈專案
+MSBuild "!SLN!" /p:platform="any cpu" /p:configuration="release" /p:DeployOnBuild=true /p:PublishProfile="FolderProfile" /verbosity:minimal
+if !errorlevel! neq 0 goto end
 echo.
-:: 最後更新時間 last-update-time
-set LST=null
-:: 透過 powershell 取得目前時間
-echo Get DateTime string...
-for /f "delims=" %%a in ('powershell -Command "Get-Date -format 'yyyy-MM-dd HH:mm:ss'"') do @set LST=%%a
-echo %LST%
+
+@REM 透過 powershell 取得目前時間
+for /f "delims=" %%a in ('powershell -Command "Get-Date -format 'yyyy-MM-dd HH:mm:ss'"') do @set UPD=%%a
+echo Get datetime: !UPD!
+@REM 設定 [Scripts] 資料夾
+if not exist "!SCRIPTS_DIR!" mkdir "!SCRIPTS_DIR!"
+@REM 將最後更新時間寫入檔案
+echo const _lims2_last_update='!UPD!';> "!UPD_FILE!"
+echo Save last-update-time '!UPD!' to [!SCRIPTS_DIR!]
+
+:end
+endlocal
 echo.
-:: TODO: [RELEASE_CODE] 發佈路徑
-set TAG=%2
-:: 指定 [Scripts] 資料夾
-set DIR="%TAG:"=%\Scripts"
-:: 檢查資料夾是否存在，不存在就新增
-if not exist %DIR% mkdir %DIR%
-:: 將最後更新時間寫入檔案
-echo const _lims2_last_update='%LST%';> %DIR%\lastUpdate.js
-echo Save last-update-time '%LST%' to [%DIR:"=%]
-echo.
-pause
