12c05ba162b87d3d993ee26b3091d9ffaab567d9
brian_li <brian.li@sgs.com>
2024-08-30 09:10:33 +0800

    修正起始 log 寫入異常

--- Utils/log_file.bat
+++ Utils/log_file.bat
@@ -12,17 +12,15 @@ if "%LogMessage%"=="" set "LogMessage=This is a test message"
 if "%LogLevel%"=="" set "LogLevel=INF"
 if "%LogFile%"=="" set "LogFile=%LOG_FILE%"
 
-if "%Timestamp%"=="" (
-    @REM 設定時間戳記 (Seq 使用 ISO 8601 格式)
-    call %~dp0get_date.bat 0 "yyyy-MM-ddTHH:mm:ss.fffZ"
-    set "Timestamp=%YMD%"
-)
-@REM @REM 設定時間戳記 (Seq 使用 ISO 8601 格式)
-@REM call %~dp0get_date.bat 0 "yyyy-MM-ddTHH:mm:ss.fffZ"
-@REM set "Timestamp=%YMD%"
+@REM 設定時間戳記 (Seq 使用 ISO 8601 格式)
+if "%Timestamp%"=="" call %~dp0get_date.bat 0 "yyyy-MM-ddTHH:mm:ss.fffZ"
 
-:: 寫入日誌
-echo [%Timestamp%] [%LogLevel%] %LogMessage% >> "%LogFile%"
+@REM 檢查目錄與檔案是否存在，不存在則建立
+if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
+if not exist "%LogFile%" type nul > "%LogFile%"
+
+@REM 寫入檔案
+echo [%YMD%] [%LogLevel%] %LogMessage% >> "%LogFile%"
 
 endlocal
 exit /b
--- Utils/logger.bat
+++ Utils/logger.bat
@@ -11,12 +11,14 @@ if "%LogMessage%"=="" set "LogMessage=This is a test message"
 if "%LogLevel%"=="" set "LogLevel=Information"
 if "%LogFile%"=="" set "LogFile=%LOG_FILE%"
 
-@REM 設定時間戳記 (Seq 使用 ISO 8601 格式)
+@REM 設定時間戳記 (ISO 8601 格式)
 call %~dp0get_date.bat 0 "yyyy-MM-ddTHH:mm:ss.fffZ"
-@REM set "Timestamp=%YMD%"
 
+@REM 寫入檔案
 call %~dp0log_file.bat "%YMD%" "%LogMessage%" "%LogLevel%" "%LogFile%"
-call %~dp0log_seq.bat "%YMD%" "%LogMessage%" "%LogLevel%" "%LogFile%"
+
+@REM 寫入 Seq
+call %~dp0log_seq.bat "%YMD%" "%LogMessage%" "%LogLevel%"
 
 endlocal
 exit /b
